476ad2a7d84a537f5738bf624e9a3e06e20929c1
cheintzelman141 <chrish@diversifiedtechnology.com>
2021-10-29 18:26:13 -0400

    Week3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import { ToastController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,98 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController ) {
 
+  }
+
+
+  title = "Grocery List"
+
+  Items = [
+    {
+      name: "bread",
+      quantity: 1
+      
+
+    },
+    {
+      name: "Milk",
+      quantity: 3
+      
+
+    },
+    {
+      name: "Cookies",
+      quantity: 2
+      
+
+    },
+    {
+      name: "Eggs",
+      quantity: 18
+      
+
+    },
+    {
+      name: "Blueberry",
+      quantity: 5
+      
+
+    }
+
+
+
+  ]
+  DeleteItem(Item,index){
+    console.log("Deleted")
+    const toast = this.toastCtrl.create({
+      message: 'Removing Item - ' + Item.name + " ...",
+      duration: 3000
+    });
+    toast.present();
+    this.Items.splice(index, 1);
+  }
+
+
+  
+  AddItem(){
+
+    console.log("Added")
+    this.ShowAddItemPrompt();
+
+  }
+
+  ShowAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Item',
+      message: "Please enter item...",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            console.log('Saved clicked', item);
+            this.Items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
